d0822b305f32c9f261b94989fd0ba6008f1cf4de
lukasz.ozdarski <lukasz.ozd@gmail.com>
2018-11-08 13:58:41 +0100

    @responsebody

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.sql.DataSource;
 
@@ -59,8 +60,8 @@ public class Main {
     }
   }
   
-  
   @RequestMapping("/rest/test")
+  @ResponseBody
   public String Test() {
 	  return "{'message' : 'success'}";
   }
